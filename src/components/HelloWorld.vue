1b4b24f222787242e09300c9fee49133b4b0753d
Matheus Silva <matheussilva.fga@gmail.com>
2022-07-27 16:36:10 -0300

    Configuring less, less loader and creating a global variables file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -40,7 +40,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="less">
 h3 {
   margin: 40px 0 0;
 }
@@ -53,6 +53,6 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: @pink;
 }
 </style>
