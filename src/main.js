fb50ae78fe4b9bf63f87baf5d44800ada287db5f
Matheus Silva <matheussilva.fga@gmail.com>
2022-08-02 12:44:16 -0300

    Configuring Vuex

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+import { store } from './store/store'
 import App from './App.vue'
 import './assets/css/global.less'
 import router from './router'
+import 'es6-promise/auto'
+
+Vue.use(Vuex)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
